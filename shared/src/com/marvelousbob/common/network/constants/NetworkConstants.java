2b904f3626792b1ae589f8bb4e5e51fd1c58d98c
payne911 <payne911@users.noreply.github.com>
2020-11-21 18:23:37 -0500

    Continuous Deployment: re-enabling AWS ECS

--- shared/src/com/marvelousbob/common/network/constants/NetworkConstants.java
+++ shared/src/com/marvelousbob/common/network/constants/NetworkConstants.java
@@ -8,7 +8,7 @@ public final class NetworkConstants {
     public static final int WRITE_BUFFER_SIZE = 65536;
     public static final int OBJECT_BUFFER_SIZE = 8192 * 4;
 
-    public static final int PORT = 80;
+    public static final int PORT = 81;
     public static final int TIMEOUT = 15000;
     public static final String REMOTE_SERVER_IP = "52.60.181.140";
 }
